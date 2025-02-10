c6946d369528e67717b3fed44b882889982198e4
PreethamMukundan <rpm869@gmail.com>
2025-02-09 19:02:26 -0800

    init GameplayAbility system
    Tested the Attribute sets
    Should test the abilities

---
+++ Source/ZeroLock/Public/GAS/BaseCharAttributeSet.h
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "BaseCharAttributeSet.generated.h"
+
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
+ * 
+ */
+UCLASS()
+class ZEROLOCK_API UBaseCharAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+		
+public:
+
+	UBaseCharAttributeSet();
+
+	// Attribute Set Overrides.
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+
+	// Set Attributes to replicate.
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	// Used to create a local copy of Damage which is then subtracted from Current Health.
+	UPROPERTY(BlueprintReadOnly, Category = "Health Attribute Set", meta = (HideFromLevelInfos))
+		FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UBaseCharAttributeSet, Damage)
+
+		// Used to create a local copy of Healing which is then added to Current Health.
+		UPROPERTY(BlueprintReadOnly, Category = "Health Attribute Set", meta = (HideFromLevelInfos))
+		FGameplayAttributeData Healing;
+	ATTRIBUTE_ACCESSORS(UBaseCharAttributeSet, Healing)
+
+		// Holds the current value for Health.
+		UPROPERTY(BlueprintReadOnly, Category = "Health Attribute Set", ReplicatedUsing = OnRep_CurrentHealth)
+		FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UBaseCharAttributeSet, CurrentHealth)
+
+		// Holds the value for Maximum Health.
+		UPROPERTY(BlueprintReadOnly, Category = "Health Attribute Set", ReplicatedUsing = OnRep_MaximumHealth)
+		FGameplayAttributeData MaximumHealth;
+	ATTRIBUTE_ACCESSORS(UBaseCharAttributeSet, MaximumHealth)
+
+		// Holds the value for Health Regeneration.
+		UPROPERTY(BlueprintReadOnly, Category = "Health Attribute Set", ReplicatedUsing = OnRep_HealthRegeneration)
+		FGameplayAttributeData HealthRegeneration;
+	ATTRIBUTE_ACCESSORS(UBaseCharAttributeSet, HealthRegeneration)
+
+		UPROPERTY(BlueprintReadOnly, Category = "Health Attribute Set", ReplicatedUsing = OnRep_CurrentSpeed)
+		FGameplayAttributeData CurrentSpeed;
+	ATTRIBUTE_ACCESSORS(UBaseCharAttributeSet, CurrentSpeed)
+
+		UPROPERTY(BlueprintReadOnly, Category = "Health Attribute Set", ReplicatedUsing = OnRep_CurrentJump)
+		FGameplayAttributeData CurrentJump;
+	ATTRIBUTE_ACCESSORS(UBaseCharAttributeSet, CurrentJump)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Soul Attribute Set", ReplicatedUsing = OnRep_CurrentJump)
+	FGameplayAttributeData Soul;
+	ATTRIBUTE_ACCESSORS(UBaseCharAttributeSet, Soul)
+
+protected:
+
+	void AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty) const;
+
+	UFUNCTION()
+		virtual void OnRep_CurrentHealth(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+		virtual void OnRep_CurrentSpeed(const FGameplayAttributeData& OldValue);
+
+
+	UFUNCTION()
+		virtual void OnRep_CurrentJump(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+		virtual void OnRep_MaximumHealth(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+		virtual void OnRep_HealthRegeneration(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_Souls(const FGameplayAttributeData& OldValue);
+	
+};
--- Source/ZeroLock/ZeroLockCharacter.cpp
+++ Source/ZeroLock/ZeroLockCharacter.cpp
@@ -1,6 +1,8 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ZeroLockCharacter.h"
+
+#include "AbilitySystemComponent.h"
 #include "Engine/LocalPlayer.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
@@ -11,6 +13,9 @@
 #include "EnhancedInputSubsystems.h"
 #include "InputActionValue.h"
 #include "ZeroBaseCharacterMovementComp.h"
+#include "GAS/BaseCharAbilitySystemComponent.h"
+#include "GAS/BaseCharAttributeSet.h"
+#include "GAS/BaseGameplayAbility.h"
 
 DEFINE_LOG_CATEGORY(LogTemplateCharacter);
 
@@ -56,6 +61,14 @@ AZeroLockCharacter::AZeroLockCharacter(const FObjectInitializer& ObjectInitializ
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+
+	//GAS
+	
+	//GAS Components
+	AbilitySystemComp = CreateDefaultSubobject<UBaseCharAbilitySystemComponent>(TEXT("AbilitySystemComp"));
+	AbilitySystemComp->SetIsReplicated(true);
+	AbilitySystemComp->SetReplicationMode(EGameplayEffectReplicationMode::Full);
+	AttributeSet = CreateDefaultSubobject<UBaseCharAttributeSet>(TEXT("AttributeSet"));
 }
 
 void AZeroLockCharacter::BeginPlay()
@@ -85,6 +98,124 @@ bool AZeroLockCharacter::CanJumpInternal_Implementation() const
 	return Super::CanJumpInternal_Implementation();
 }
 
+class UAbilitySystemComponent* AZeroLockCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComp;
+}
+
+void AZeroLockCharacter::InitializeAttributes()
+{
+	if (AbilitySystemComp && DefaultGameplayEffect)
+	{
+		FGameplayEffectContextHandle EffectContext = AbilitySystemComp->MakeEffectContext();
+		EffectContext.AddSourceObject(this);
+
+
+		FGameplayEffectSpecHandle SpecHandle = AbilitySystemComp->MakeOutgoingSpec(DefaultGameplayEffect, 1, EffectContext);
+
+		if (SpecHandle.IsValid())
+		{
+			FActiveGameplayEffectHandle GEHandle = AbilitySystemComp->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+		}
+	}
+}
+
+void AZeroLockCharacter::GiveAbilities()
+{
+	if (HasAuthority() && AbilitySystemComp)
+	{
+		for (TSubclassOf<UBaseGameplayAbility>& StartupAbility : DefaultAbilities)
+		{
+			DefaultAbilitiesHandles.Add(
+			AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(StartupAbility, 1, static_cast<int32>(StartupAbility.GetDefaultObject()->AbilityInputID), this)));
+		}
+		if (PrimaryFireAbility)
+		{
+			DefaultAbilitiesHandles.Add(
+				AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(PrimaryFireAbility, 1, static_cast<int32>(PrimaryFireAbility.GetDefaultObject()->AbilityInputID), this)));
+		}
+		if (SecondryFireAbility)
+		{
+			DefaultAbilitiesHandles.Add(
+				AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(SecondryFireAbility, 1, static_cast<int32>(SecondryFireAbility.GetDefaultObject()->AbilityInputID), this)));
+		}
+		if (Ability_1)
+		{
+			DefaultAbilitiesHandles.Add(
+				AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(Ability_1, 1, static_cast<int32>(Ability_1.GetDefaultObject()->AbilityInputID), this)));
+		}
+		if (Ability_2)
+		{
+			DefaultAbilitiesHandles.Add(
+				AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(Ability_2, 1, static_cast<int32>(Ability_2.GetDefaultObject()->AbilityInputID), this)));
+		}
+		if (UltimateAbility)
+		{
+			DefaultAbilitiesHandles.Add(
+				AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(UltimateAbility, 1, static_cast<int32>(UltimateAbility.GetDefaultObject()->AbilityInputID), this)));
+		}
+	}
+	if (HasAuthority() && AbilitySystemComp)
+	{
+		for (TSubclassOf<UBaseGameplayAbility>& StartupAbility : DefaultAbilities)
+		{
+
+			AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(StartupAbility, 1, static_cast<int32>(StartupAbility.GetDefaultObject()->AbilityInputID), this));
+
+
+		}
+	}
+}
+
+void AZeroLockCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	//server GAS 
+	AbilitySystemComp->InitAbilityActorInfo(this, this);
+
+	InitializeAttributes();
+	GiveAbilities();
+}
+
+void AZeroLockCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	//Client GAS
+	AbilitySystemComp->InitAbilityActorInfo(this, this);
+
+	InitializeAttributes();
+}
+
+void AZeroLockCharacter::PrimaryFirePressed()
+{
+
+}
+
+void AZeroLockCharacter::PrimaryFireReleased()
+{
+}
+
+void AZeroLockCharacter::PrimaryFireTickFunction()
+{
+}
+
+void AZeroLockCharacter::SecondryFirePressed()
+{
+}
+
+void AZeroLockCharacter::Ability_1Pressed()
+{
+}
+
+void AZeroLockCharacter::Ability_2Pressed()
+{
+}
+
+void AZeroLockCharacter::UltimateAbilityPressed()
+{
+}
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -118,9 +249,19 @@ void AZeroLockCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 	{
 		UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
 	}
+	
+	if (AbilitySystemComp && InputComponent)
+	{
+		const FGameplayAbilityInputBinds Binds("Confirm", "Cancel",  FTopLevelAssetPath(GetPathNameSafe(UClass::TryFindTypeSlow<UEnum>("EGASAbilityInputID"))), static_cast<int32>(EGASAbilityInputID::Confirm), static_cast<int32>(EGASAbilityInputID::Cancel));
+
+		AbilitySystemComp->BindAbilityActivationToInputComponent(InputComponent, Binds);
+	}
 }
 
-
+void AZeroLockCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+}
 
 
 void AZeroLockCharacter::Move(const FInputActionValue& Value)
